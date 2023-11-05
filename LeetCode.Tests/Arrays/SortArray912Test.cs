ac7288d5de859425eeefbc42304360dd80e4c118
João Fabio Lourenço dos Santos <fabio.jfl@hotmail.com>
2023-11-04 21:41:22 -0300

    Implemented Insertion Sort Algorithm.

---
+++ LeetCode.Tests/Arrays/SortArray912Test.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Arrays;
+
+namespace LeetCode.Tests.Arrays;
+
+public class SortArray912Test
+{
+    [Fact]
+    public void ShouldSortArray()
+    {
+        // Arrange
+        int[] nums = new int[] { 5, 2, 3, 1 };
+        int[] expected = new int[] { 1, 2, 3, 5 };
+
+        SortArray912.SortArray(nums);
+        Assert.Equal(expected, nums);
+    }
+
+    [Fact]
+    public void ShouldSortArrayWithDuplicatedValues()
+    {
+        // Arrange
+        int[] nums = new int[] { 5, 1, 1, 2, 0, 0 };
+        int[] expected = new int[] { 0, 0, 1, 1, 2, 5 };
+
+        SortArray912.SortArray(nums);
+        Assert.Equal(expected, nums);
+    }
+}
---
+++ LeetCodeProblems/Arrays/SortArray912.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Arrays;
+
+public class SortArray912
+{
+    /// <summary>
+    /// Insertion Sort Algorithm: O(n2)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public static int[] SortArray(int[] nums)
+    {
+        for (int i = 1; i < nums.Length; i++)
+        {
+            int j = i;
+
+            while (j > 0 && nums[j] < nums[j - 1])
+            {
+                int temp = nums[j];
+                nums[j] = nums[j - 1];
+                nums[j - 1] = temp;
+                j--;
+            }
+        }
+
+        return nums;
+    }
+}
