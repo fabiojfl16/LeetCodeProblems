99eee2155efb18748f719dc11ae657d524f65258
Santos Joao (7) <joao.santos.7@volvo.com>
2023-11-06 18:11:29 -0300

    Implemented 347 Top K Frequent Elements.

---
+++ LeetCode.Tests/Arrays/TopKFrequestElements347Test.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Arrays;
+
+namespace LeetCode.Tests.Arrays;
+
+public class TopKFrequestElements347Test
+{
+    [Fact]
+    public void ShouldReturn1and2AsMostFrequentNumbers()
+    {
+        // Arrange
+        int[] nums = { 1, 1, 1, 2, 2, 3 };
+        int numberOfElements = 2;
+
+        // Act
+        var topK = new TopKFrequestElements347();
+        int[] result = topK.TopKFrequent(nums, numberOfElements);
+
+        // Assert
+        Assert.Equal(1, result[0]);
+        Assert.Equal(2, result[1]);
+    }
+
+    [Fact]
+    public void ShouldReturn1AsMostFrequentNumber()
+    {
+        // Arrange
+        int[] nums = { 1 };
+        int numberOfElements = 1;
+
+        // Act
+        var topK = new TopKFrequestElements347();
+        int[] result = topK.TopKFrequent(nums, numberOfElements);
+
+        // Assert
+        Assert.Equal(1, result[0]);
+    }
+}
---
+++ LeetCodeProblems/Arrays/TopKFrequestElements347.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Arrays;
+
+public class TopKFrequestElements347
+{
+    public int[] TopKFrequent(int[] nums, int numberOfElements)
+    {
+        var numbersMap = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!numbersMap.ContainsKey(nums[i]))
+            {
+                numbersMap.Add(nums[i], 0);
+            }
+
+            numbersMap[nums[i]]++;
+        }
+
+        return numbersMap
+                .OrderByDescending(number => number.Value)
+                .Select(pair => pair.Key)
+                .Take(numberOfElements)
+                .ToArray();
+    }
+}
