14c2f5b314e67cc5ff6698c03bf914c912646a16
Santos Joao (7) <joao.santos.7@volvo.com>
2023-11-06 17:36:49 -0300

    Tests for GroupAnagrams49.

---
+++ LeetCode.Tests/Arrays/GroupAnagrams49Test.cs
@@ -0,0 +1,60 @@
+﻿using LeetCode.Arrays;
+
+namespace LeetCode.Tests.Arrays;
+
+public class GroupAnagrams49Test
+{
+    [Fact]
+    public void ShouldGroupAnagrams()
+    {
+        // Arrange
+        var strs = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+
+        // Act
+        var groupAnagram = new GroupAnagrams49();
+        var groups = groupAnagram.GroupAnagrams(strs);
+
+        // Assert
+        Assert.Equal(3, groups.Count);
+        
+        Assert.Equal("eat", groups[0][0]);
+        Assert.Equal("tea", groups[0][1]);
+        Assert.Equal("ate", groups[0][2]);
+
+        Assert.Equal("tan", groups[1][0]);
+        Assert.Equal("nat", groups[1][1]);
+
+        Assert.Equal("bat", groups[2][0]);
+    }
+
+
+    [Fact]
+    public void ShouldGroupEmptyString()
+    {
+        // Arrange
+        var strs = new string[] { "" };
+
+        // Act
+        var groupAnagram = new GroupAnagrams49();
+        var groups = groupAnagram.GroupAnagrams(strs);
+
+        // Assert
+        Assert.Equal(1, groups.Count);
+        Assert.Equal("", groups[0][0]);
+    }
+
+    [Fact]
+    public void ShouldGroupSingleString()
+    {
+        // Arrange
+        var strs = new string[] { "a" };
+
+        // Act
+        var groupAnagram = new GroupAnagrams49();
+        var groups = groupAnagram.GroupAnagrams(strs);
+
+        // Assert
+        Assert.Equal(1, groups.Count);
+        Assert.Equal("a", groups[0][0]);
+    }
+}
---
+++ LeetCodeProblems/Arrays/GroupAnagrams49.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Arrays;
+
+public class GroupAnagrams49
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        var groups = new Dictionary<string, IList<string>>();
+
+        foreach (string word in strs)
+        {
+            string wordLowerCase = word.ToLower();
+            char[] hash = new char[26];
+            
+            foreach (char character in wordLowerCase)
+            {
+                hash[character - 'a']++;
+            }
+
+            string key = new string(hash);
+            
+            if (!groups.ContainsKey(key))
+            {
+                groups[key] = new List<string>();
+            }
+            
+            groups[key].Add(wordLowerCase);
+        }
+
+        return groups.Values.ToList();
+    }
+}
