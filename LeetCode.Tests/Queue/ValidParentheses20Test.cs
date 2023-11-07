145607fe570373ddccaae737563935e333785a8d
Santos Joao (7) <joao.santos.7@volvo.com>
2023-11-07 15:02:50 -0300

    Tests for ValidParentheses20.

---
+++ LeetCode.Tests/Queue/ValidParentheses20Test.cs
@@ -0,0 +1,62 @@
+﻿using LeetCode.Queue;
+
+namespace LeetCode.Tests.Queue;
+
+public class ValidParentheses20Test
+{
+    [Fact]
+    public void ShouldValidateParenthesis()
+    {
+        // Arrange
+        string input = "()";
+
+        // Act
+        var valid = new ValidParentheses20();
+        bool isValid = valid.IsValid(input);
+
+        // Assert
+        Assert.True(isValid);
+    }
+
+    [Fact]
+    public void ShouldValidateMultipleItems()
+    {
+        // Arrange
+        string input = "()[]{}";
+
+        // Act
+        var valid = new ValidParentheses20();
+        bool isValid = valid.IsValid(input);
+
+        // Assert
+        Assert.True(isValid);
+    }
+
+    [Fact]
+    public void ShouldNotValidateItems()
+    {
+        // Arrange
+        string input = "(]";
+
+        // Act
+        var valid = new ValidParentheses20();
+        bool isValid = valid.IsValid(input);
+
+        // Assert
+        Assert.False(isValid);
+    }
+
+    [Fact]
+    public void ShouldValidateNestedItems()
+    {
+        // Arrange
+        string input = "([{}])";
+
+        // Act
+        var valid = new ValidParentheses20();
+        bool isValid = valid.IsValid(input);
+
+        // Assert
+        Assert.True(isValid);
+    }
+}
---
+++ LeetCodeProblems/Queue/ValidParentheses20.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Queue;
+
+public class ValidParentheses20
+{
+    public bool IsValid(string input)
+    {
+        var stack = new Stack<char>();
+        var closeToOpenMap = new Dictionary<char, char>()
+        {
+            { ')', '(' },
+            { ']', '[' },
+            { '}', '{' }
+        };
+
+        foreach (char c in input)
+        {
+            if (!closeToOpenMap.ContainsKey(c))
+            {
+                // Adds only open items to the stack: '(', '{' or '['.
+                stack.Push(c);
+                continue;
+            }
+            else if (stack.Count == 0)
+            {
+                // If stack is empty that means the 'Open' item was not added before.
+                return false;
+            }
+
+            // '(', '{' or '['
+            var lastOpenItem = stack.Pop();
+
+            // '(', '{' or '['
+            var mapValue = closeToOpenMap[c];
+
+            if (lastOpenItem != mapValue)
+                return false;
+        }
+
+        return stack.Count == 0;
+    }
+}
