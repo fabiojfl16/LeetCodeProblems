747ebce36cdc8b2704cfd4952ec927cc31b541da
Santos Joao (7) <joao.santos.7@volvo.com>
2023-11-07 12:08:36 -0300

    Tests for BaseballGame682.

---
+++ LeetCode.Tests/Stacks/BaseballGame682Test.cs
@@ -0,0 +1,51 @@
+﻿using LeetCode.Stacks;
+
+namespace LeetCode.Tests.Stacks;
+
+public class BaseballGame682Test
+{
+    [Fact]
+    public void ShouldComputeResultsAndReturn30()
+    {
+        // Arrange
+        string[] ops = new string[] { "5", "2", "C", "D", "+" };
+        int expected = 30;
+
+        // Act
+        var game = new BaseballGame682();
+        int actual = game.CalPoints(ops);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void ShouldComputeResultsAndReturn27()
+    {
+        // Arrange
+        string[] ops = new string[] { "5", "-2", "4", "C", "D", "9", "+", "+" };
+        int expected = 27;
+
+        // Act
+        var game = new BaseballGame682();
+        int actual = game.CalPoints(ops);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void ShouldComputeResultsAndReturn0()
+    {
+        // Arrange
+        string[] ops = new string[] { "1", "C" };
+        int expected = 0;
+
+        // Act
+        var game = new BaseballGame682();
+        int actual = game.CalPoints(ops);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
--- LeetCodeProblems/Stacks/BaseballGame682.cs
+++ LeetCodeProblems/Stacks/BaseballGame682.cs
@@ -1,39 +1,43 @@
 ﻿namespace LeetCode.Stacks;
 
-internal class BaseballGame682
+public class BaseballGame682
 {
-    public static int CalPoints(string[] operations)
+    public int CalPoints(string[] operations)
     {
         Stack<int> stack = new();
 
         for (int i = 0; i < operations.Length; i++)
         {
-            string operation = operations[i];
+            string op = operations[i];
 
-            if (operation == "+")
+            if(op == "+")
             {
-                int first = stack.Pop();
+                // Sum 2 previous values
                 int second = stack.Pop();
+                int first = stack.Pop();
+                int sum = second + first;
 
-                int value = first + second;
-
-                stack.Push(second);
                 stack.Push(first);
-                stack.Push(value);
+                stack.Push(second);
+                stack.Push(sum);
             }
-            else if (operation == "D")
+            else if (op == "C")
             {
-                int lastRecord = stack.Pop();
-                stack.Push(lastRecord);
-                stack.Push(2 * lastRecord);
-            }
-            else if (operation == "C")
-            {
+                // Remove last added item
                 stack.Pop();
+            }
+            else if (op == "D")
+            {
+                // Multiply last item by 2
+                int lastNumber = stack.Pop();
+                stack.Push(lastNumber);
+                stack.Push(lastNumber * 2);
             }
             else
             {
-                stack.Push(int.Parse(operation));
+                // It's a number, so add it to the stack.
+                int value = int.Parse(op);
+                stack.Push(value);
             }
         }
 
