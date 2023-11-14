c699568bbb913f0b8a5374be2052e570f60f5f68
Santos Joao (7) <joao.santos.7@volvo.com>
2023-11-14 15:46:34 -0300

    Implemented 700. Search in a Binary Search Tree.

---
+++ LeetCode.Tests/BinaryTree/SearchInBinarySearchTree700Test.cs
@@ -0,0 +1,46 @@
+﻿using LeetCode.BinaryTree;
+
+namespace LeetCode.Tests.BinaryTree;
+
+public class SearchInBinarySearchTree700Test
+{
+    [Fact]
+    public void ShouldFindValueInTheBinarySearchTree()
+    {
+        // Arrange
+        var bst = new SearchInBinarySearchTree700();
+        int expected = 2;
+        int left = 1;
+        int right = 3;
+
+        var tree = new TreeNode(4, 
+            new TreeNode(2, new TreeNode(1), new TreeNode(3)), 
+            new TreeNode(7, null));
+
+        // Act
+        var result = bst.Search(tree, expected);
+
+        // Assert
+        Assert.Equal(expected, result.val);
+        Assert.Equal(left, result.left!.val);
+        Assert.Equal(right, result.right!.val);
+    }
+
+    [Fact]
+    public void ShouldNotFindValueInTheBinarySearchTree()
+    {
+        // Arrange
+        var bst = new SearchInBinarySearchTree700();
+        int expected = 5;
+
+        var tree = new TreeNode(4,
+            new TreeNode(2, new TreeNode(1), new TreeNode(3)),
+            new TreeNode(7, null));
+
+        // Act
+        var result = bst.Search(tree, expected);
+
+        // Assert
+        Assert.Null(result);
+    }
+}
---
+++ LeetCodeProblems/BinaryTree/SearchInBinarySearchTree700.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.BinaryTree;
+
+public class SearchInBinarySearchTree700
+{
+    public TreeNode Search(TreeNode? root, int val)
+    {
+        if (root == null || root.val == val)
+            return root;
+
+        if (root.val > val)
+            return Search(root.left, val);
+        else
+            return Search(root.right, val);
+    }
+}
