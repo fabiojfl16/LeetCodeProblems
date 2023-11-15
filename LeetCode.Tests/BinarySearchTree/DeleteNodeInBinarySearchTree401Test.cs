99c5eb14f04dee728846a0a583920803ff494113
Santos Joao (7) <joao.santos.7@volvo.com>
2023-11-15 18:50:33 -0300

    Implemented 450. Delete Node in BST.

---
+++ LeetCode.Tests/BinarySearchTree/DeleteNodeInBinarySearchTree401Test.cs
@@ -0,0 +1,75 @@
+﻿using LeetCode.BinaryTree;
+
+namespace LeetCode.Tests.BinarySearchTree;
+
+public class DeleteNodeInBinarySearchTree401Test
+{
+    [Fact]
+    public void ShouldDeleteValueInTheLeftSideOfBinarySearchTree()
+    {
+        // Arrange
+        int val = 3;
+        int expectedValue = 2;
+
+        var tree = 
+            new TreeNode(5,
+                new TreeNode(3,
+                    new TreeNode(2), new TreeNode(4)),
+                new TreeNode(6, 
+                    null, new TreeNode(7))
+            );
+
+        // Act
+        var delete = new DeleteNodeInBinarySearchTree401();
+        var result = delete.DeleteNode(tree, val);
+
+        // Assert
+        Assert.Equal(expectedValue, result!.left!.val);
+    }
+
+    [Fact]
+    public void ShouldDeleteValueInTheRightSideOfBinarySearchTree()
+    {
+        // Arrange
+        int val = 3;
+        int expectedValue = 2;
+
+        var tree =
+            new TreeNode(5,
+                new TreeNode(3,
+                    new TreeNode(2), new TreeNode(4)),
+                new TreeNode(6,
+                    null, new TreeNode(7))
+            );
+
+        // Act
+        var delete = new DeleteNodeInBinarySearchTree401();
+        var result = delete.DeleteNode(tree, val);
+
+        // Assert
+        Assert.Equal(expectedValue, result!.left!.val);
+    }
+
+    [Fact]
+    public void ShouldDeleteTheRootValueOfBinarySearchTree()
+    {
+        // Arrange
+        int val = 5;
+        int expectedValue = 6;
+
+        var tree =
+            new TreeNode(5,
+                new TreeNode(3,
+                    new TreeNode(2), new TreeNode(4)),
+                new TreeNode(6,
+                    null, new TreeNode(7))
+            );
+
+        // Act
+        var delete = new DeleteNodeInBinarySearchTree401();
+        var result = delete.DeleteNode(tree, val);
+
+        // Assert
+        Assert.Equal(expectedValue, result!.val);
+    }
+}
---
+++ LeetCodeProblems/BinarySearchTree/DeleteNodeInBinarySearchTree401.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.BinaryTree;
+
+public class DeleteNodeInBinarySearchTree401
+{
+    public TreeNode? DeleteNode(TreeNode? root, int val)
+    {
+        if (root == null)
+            return null;
+
+        if(val > root.val)
+            root.right = DeleteNode(root.right, val);
+        else if(val < root.val)
+            root.left = DeleteNode(root.left, val);
+        else
+        {
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+            else
+            {
+                TreeNode minNode = GetMinValueNode(root.right);
+                root.val = minNode.val;
+                root.right = DeleteNode(root.right, minNode.val);
+            }
+        }
+
+        return root;
+    }
+
+    private TreeNode GetMinValueNode(TreeNode root)
+    {
+        var current = root;
+
+        while (current.left != null)
+            current = current.left;
+
+        return current;
+    }
+}
